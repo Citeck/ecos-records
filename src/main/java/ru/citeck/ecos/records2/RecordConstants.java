7657a5cd5f28b4d19f8869da9c5527183c371599
Pavel Simonov <Sipaha@bk.ru>
2019-04-24 11:46:40 +0700

    Add RecordConstants

---
+++ src/main/java/ru/citeck/ecos/records2/RecordConstants.java
@@ -0,0 +1,13 @@
+package ru.citeck.ecos.records2;
+
+public final class RecordConstants {
+
+    private RecordConstants() {}
+
+    public static final String ATT_PARENT = "_parent";
+    public static final String ATT_PARENT_ATT = "_parentAtt";
+    public static final String ATT_TYPE = "_type";
+    public static final String ATT_FORM_KEY = "_formKey";
+    public static final String ATT_VIEW_FORM_KEY = "_viewFormKey";
+    public static final String ATT_FORM_MODE = "_formMode";
+}

b219c171362d1cdb237467ace7000997dc94cf50
Pavel Simonov <pavel.simonov@citeck.ru>
2019-09-12 09:45:46 +0700

    Add content attribute name

--- src/main/java/ru/citeck/ecos/records2/RecordConstants.java
+++ src/main/java/ru/citeck/ecos/records2/RecordConstants.java
@@ -8,13 +8,20 @@ public final class RecordConstants {
     public static final String ATT_PARENT_ATT = "_parentAtt";
     public static final String ATT_TYPE = "_type";
     public static final String ATT_FORM_KEY = "_formKey";
-    public static final String ATT_VIEW_FORM_KEY = "_viewFormKey";
     public static final String ATT_FORM_MODE = "_formMode";
     public static final String ATT_ALIAS = "_alias";
     public static final String ATT_DASHBOARD_KEY = "_dashboardKey";
     public static final String ATT_DASHBOARD_TYPE = "_dashboardType";
     public static final String ATT_MODIFIED = "_modified";
+    public static final String ATT_CONTENT = "_content";
 
     public static final String FORM_MODE_CREATE = "CREATE";
     public static final String FORM_MODE_EDIT = "EDIT";
+
+    /**
+     * Deprecated attribute.
+     * @deprecated view form defined by mode. Not by this attribute
+     */
+    @Deprecated
+    public static final String ATT_VIEW_FORM_KEY = "_viewFormKey";
 }

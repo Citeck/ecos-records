4b205d21fc7691f49834688923d92c9cbcb264a2
Pavel Simonov <pavel.simonov@citeck.ru>
2019-12-13 11:48:33 +0700

    Add evaluators registrar

---
+++ ecos-records-spring/src/main/java/ru/citeck/ecos/records2/spring/RecordEvaluatorsRegistrar.java
@@ -0,0 +1,39 @@
+package ru.citeck.ecos.records2.spring;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.citeck.ecos.records2.evaluator.RecordEvaluator;
+import ru.citeck.ecos.records2.evaluator.RecordEvaluatorsService;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RecordEvaluatorsRegistrar {
+
+    private List<RecordEvaluator<?, ?, ?>> evaluators;
+    private final RecordEvaluatorsService recordEvaluatorsService;
+
+    @PostConstruct
+    public void register() {
+        log.info("========================== RecordEvaluatorsRegistrar ==========================");
+        if (evaluators != null) {
+            evaluators.forEach(this::register);
+        }
+        log.info("========================= /RecordEvaluatorsRegistrar ==========================");
+    }
+
+    private void register(RecordEvaluator<?, ?, ?> evaluator) {
+        log.info("Register: \"" + evaluator.getId() + "\" with class " + evaluator.getClass().getName());
+        recordEvaluatorsService.register(evaluator);
+    }
+
+    @Autowired(required = false)
+    public void setEvaluators(List<RecordEvaluator<?, ?, ?>> evaluators) {
+        this.evaluators = evaluators;
+    }
+}

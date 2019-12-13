207bc4d51f19a64086e10839704c8637b5bd492d
Pavel Simonov <pavel.simonov@citeck.ru>
2019-12-13 12:10:50 +0700

    Add evaluator javadoc

--- ecos-records/src/main/java/ru/citeck/ecos/records2/evaluator/RecordEvaluator.java
+++ ecos-records/src/main/java/ru/citeck/ecos/records2/evaluator/RecordEvaluator.java
@@ -9,6 +9,7 @@ public interface RecordEvaluator<ReqMetaT, EvalMetaT, ConfigT> {
 
     /**
      * Get attributes which is required for Evaluator.
+     *
      * @param config evaluator configuration
      * @return Map&lt;String, String&gt; or meta class instance or null if attributes is not required
      */
@@ -16,25 +17,28 @@ public interface RecordEvaluator<ReqMetaT, EvalMetaT, ConfigT> {
 
     /**
      * Meta type for conversion of received attributes. Used for type safety.
+
      * @return DTO class or null
      */
     Class<EvalMetaT> getEvalMetaType();
 
     /**
      * Evaluator config type. Used for type safety.
+     *
      * @return config class or null
      */
     Class<ConfigT> getConfigType();
 
     /**
      * Evaluate result by meta and config.
+     *
      * @param meta metadata received from recordRef
      * @param config evaluator config
      */
     boolean evaluate(EvalMetaT meta, ConfigT config);
 
     /**
-     * Get evaluator ID
+     * Get evaluator ID.
      */
     String getId();
 }

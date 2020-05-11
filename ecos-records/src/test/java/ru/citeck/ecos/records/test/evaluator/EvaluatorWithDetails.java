06598c71b13778830da39e28df40e9ef63d75cfa
Pavel Simonov <pavel.simonov@citeck.ru>
2020-05-12 02:51:39 +0700

    Fix evaluator with details test

--- ecos-records/src/test/java/ru/citeck/ecos/records/test/evaluator/EvaluatorWithDetails.java
+++ ecos-records/src/test/java/ru/citeck/ecos/records/test/evaluator/EvaluatorWithDetails.java
@@ -1,23 +1,23 @@
 package ru.citeck.ecos.records.test.evaluator;
 
+import kotlin.Unit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.citeck.ecos.records2.evaluator.details.*;
-import ru.citeck.ecos.records2.graphql.meta.value.MetaValue;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EvaluatorWithDetails implements RecordEvaluatorWithDetails<Object,
-                                                                        MetaValue,
+                                                                        Unit,
                                                                         EvaluatorWithDetails.Config> {
 
     public static final String TYPE = "with-details";
 
     @Override
-    public EvalDetails evalWithDetails(MetaValue meta, Config config) {
+    public EvalDetails evalWithDetails(Unit meta, Config config) {
 
         List<EvalResultCause> causes = Arrays.stream(config.cause)
             .map(EvalResultCauseImpl::new)

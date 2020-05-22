01bd433d859fe24b38478d4914180cfe34f3a2fc
Pavel Simonov <pavel.simonov@citeck.ru>
2020-05-22 08:11:06 +0700

    Fix Map attribute for DtoMetaResolver

--- ecos-records/src/main/java/ru/citeck/ecos/records2/meta/DtoMetaResolver.java
+++ ecos-records/src/main/java/ru/citeck/ecos/records2/meta/DtoMetaResolver.java
@@ -66,7 +66,8 @@ public class DtoMetaResolver {
             new ScalarField<>(ArrayNode.class, "json"),
             new ScalarField<>(ObjectData.class, "json"),
             new ScalarField<>(DataValue.class, "json"),
-            new ScalarField<>(RecordRef.class, "id")
+            new ScalarField<>(RecordRef.class, "id"),
+            new ScalarField<>(Map.class, "json")
         ).forEach(s -> scalars.put(s.getFieldType(), s));
 
         if (LibsUtils.isJacksonPresent()) {

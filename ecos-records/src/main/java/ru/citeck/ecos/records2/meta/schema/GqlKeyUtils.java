2d530dbd86c308f9d9d84b326f9f9a374fc8bbdd
Pavel Simonov <pavel.simonov@citeck.ru>
2020-09-21 12:59:00 +0700

    Add 'as'and 'has' attributes to parser

--- ecos-records/src/main/java/ru/citeck/ecos/records2/meta/schema/GqlKeyUtils.java
+++ ecos-records/src/main/java/ru/citeck/ecos/records2/meta/schema/GqlKeyUtils.java
@@ -1,16 +1,26 @@
 package ru.citeck.ecos.records2.meta.schema;
 
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GqlKeyUtils {
 
-    private static final String SPECIAL_CHARS = ":{}(),.?-+\"' ";
+    private static final char[] SPECIAL_CHARS = ":{}(),.?-+\"' ".toCharArray();
     private static final Pattern SPECIAL_CHAR_PATTERN = Pattern.compile("_u([A-Fa-f\\d]{4})_");
 
-    private static String toHex(char ch) {
-        String code = Integer.toHexString(ch).toUpperCase();
-        return "0000".substring(code.length()) + code;
+    private static final String[] SPECIAL_CHARS_HEX;
+
+    static {
+        Function<Character, String> toHex = ch -> {
+            String code = Integer.toHexString(ch).toUpperCase();
+            return "_u" + "0000".substring(code.length()) + code + "_";
+        };
+
+        SPECIAL_CHARS_HEX = new String[SPECIAL_CHARS.length];
+        for (int i = 0; i < SPECIAL_CHARS.length; i++) {
+            SPECIAL_CHARS_HEX[i] = toHex.apply(SPECIAL_CHARS[i]);
+        }
     }
 
     private static char toChar(String hex) {
@@ -18,12 +28,11 @@ public class GqlKeyUtils {
     }
 
     public static String escape(String str) {
-        for (int i = 0; i < SPECIAL_CHARS.length(); i++) {
-            char ch = SPECIAL_CHARS.charAt(i);
+        for (int i = 0; i < SPECIAL_CHARS.length; i++) {
+            char ch = SPECIAL_CHARS[i];
             String chStr = "" + ch;
             if (str.contains(chStr)) {
-                String hexStr = toHex(ch);
-                str = str.replace(chStr, "_u" + hexStr + "_");
+                str = str.replace(chStr, SPECIAL_CHARS_HEX[i]);
             }
         }
         return str;

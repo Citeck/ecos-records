9f662474a6db02044f4cffb82e7fef2a24ec8657
Pavel Simonov <pavel.simonov@citeck.ru>
2020-10-27 07:58:25 +0700

    Kotlin migration

--- ecos-records/src/main/java/ru/citeck/ecos/records2/RecordRef.kt
+++ ecos-records/src/main/java/ru/citeck/ecos/records2/RecordRef.kt
@@ -1,205 +1,183 @@
-package ru.citeck.ecos.records2;
+package ru.citeck.ecos.records2
 
-import ecos.com.fasterxml.jackson210.annotation.JsonCreator;
-import ecos.com.fasterxml.jackson210.annotation.JsonValue;
-import org.jetbrains.annotations.NotNull;
-import ru.citeck.ecos.commons.utils.StringUtils;
-
-import java.io.Serializable;
-import java.util.Objects;
+import ecos.com.fasterxml.jackson210.annotation.JsonCreator
+import ecos.com.fasterxml.jackson210.annotation.JsonValue
+import ru.citeck.ecos.commons.utils.StringUtils.containsOnly
+import ru.citeck.ecos.commons.utils.StringUtils.isBlank
+import ru.citeck.ecos.commons.utils.StringUtils.isNotBlank
+import java.io.Serializable
+import java.util.*
 
 /**
  * Unique identifier of a record.
  */
-public class RecordRef implements Serializable {
+class RecordRef : Serializable {
 
-    private static final long serialVersionUID = 1L;
+    companion object {
 
-    public static final RecordRef EMPTY = new RecordRef();
+        private const val serialVersionUID = 1L
 
-    public static final String SOURCE_DELIMITER = "@";
-    public static final String APP_NAME_DELIMITER = "/";
+        @JvmField
+        val EMPTY = RecordRef()
 
-    private final String appName;
-    private final String sourceId;
-    private final String id;
+        const val SOURCE_DELIMITER = "@"
+        const val APP_NAME_DELIMITER = "/"
 
-    private RecordRef() {
-        appName = "";
-        sourceId = "";
-        id = "";
-    }
-
-    private RecordRef(String appName, String sourceId, String id) {
-        this.id = StringUtils.isNotBlank(id) ? id : "";
-        this.appName = StringUtils.isNotBlank(appName) ? appName : "";
-        this.sourceId = StringUtils.isNotBlank(sourceId) ? sourceId : "";
-    }
-
-    @NotNull
-    public static RecordRef create(String sourceId, RecordRef id) {
-        return create(sourceId, id.toString());
-    }
-
-    @NotNull
-    public static RecordRef create(String sourceId, String id) {
-        return create("", sourceId, id);
-    }
-
-    @NotNull
-    public static RecordRef create(String appName, String sourceId, String id) {
-        if (StringUtils.isBlank(appName) && StringUtils.isBlank(sourceId) && isBlankId(id)) {
-            return EMPTY;
-        }
-        return new RecordRef(
-                appName != null ? appName : "",
-                sourceId != null ? sourceId : "",
-                isBlankId(id) ? "" : id
-        );
-    }
-
-    public static boolean isNotEmpty(RecordRef ref) {
-        return !isEmpty(ref);
-    }
-
-    public static boolean isEmpty(RecordRef ref) {
-        return ref == null
-            || ref == RecordRef.EMPTY
-            || StringUtils.isBlank(ref.getId())
-                && StringUtils.isBlank(ref.getSourceId())
-                && StringUtils.isBlank(ref.getAppName());
-    }
-
-    @NotNull
-    public RecordRef addAppName(String appName) {
-        String current = toString();
-        if (current.contains("@")) {
-            return valueOf(appName + "/" + current);
-        } else {
-            return valueOf(appName + "/@" + current);
-        }
-    }
-
-    @NotNull
-    public RecordRef withDefaultAppName(String appName) {
-        if (StringUtils.isBlank(appName)) {
-            return this;
-        }
-        return !this.appName.isEmpty() ? this : addAppName(appName);
-    }
-
-    @NotNull
-    public RecordRef removeAppName() {
-        if (appName.isEmpty()) {
-            return this;
-        }
-        return create(sourceId, id);
-    }
-
-    @NotNull
-    @JsonCreator
-    @com.fasterxml.jackson.annotation.JsonCreator
-    public static RecordRef valueOf(String recordRefStr) {
-
-        if (isBlankId(recordRefStr)) {
-            return EMPTY;
+        @JvmStatic
+        fun create(sourceId: String?, id: RecordRef): RecordRef {
+            return create(sourceId, id.toString())
         }
 
-        String id;
-        String appName;
-        String sourceId;
+        @JvmStatic
+        fun create(sourceId: String?, id: String?): RecordRef {
+            return create("", sourceId, id)
+        }
 
-        int sourceDelimIdx = recordRefStr.indexOf(SOURCE_DELIMITER);
+        @JvmStatic
+        fun create(appName: String?, sourceId: String?, id: String?): RecordRef {
+            return if (isBlank(appName) && isBlank(sourceId) && isBlankId(id)) {
+                EMPTY
+            } else RecordRef(
+                appName ?: "",
+                sourceId ?: "",
+                (if (isBlankId(id)) "" else id)!!
+            )
+        }
 
-        if (sourceDelimIdx != -1) {
+        @JvmStatic
+        fun isNotEmpty(ref: RecordRef?): Boolean {
+            return !isEmpty(ref)
+        }
 
-            id = recordRefStr.substring(sourceDelimIdx + 1);
+        @JvmStatic
+        fun isEmpty(ref: RecordRef?): Boolean {
+            return ref == null || ref === EMPTY
+                || (isBlank(ref.id)
+                    && isBlank(ref.sourceId)
+                    && isBlank(ref.appName))
+        }
 
-            String source = recordRefStr.substring(0, sourceDelimIdx);
-            int appNameDelimIdx = source.indexOf(APP_NAME_DELIMITER);
-            if (appNameDelimIdx > -1) {
-                appName = source.substring(0, appNameDelimIdx);
-                sourceId = source.substring(appNameDelimIdx + 1);
-            } else {
-                appName = "";
-                sourceId = source;
+        @JvmStatic
+        @JsonCreator
+        @com.fasterxml.jackson.annotation.JsonCreator
+        fun valueOf(recordRefStr: String?): RecordRef {
+            if (recordRefStr == null || isBlankId(recordRefStr)) {
+                return EMPTY
             }
+            val id: String
+            val appName: String
+            val sourceId: String
+            val sourceDelimIdx = recordRefStr.indexOf(SOURCE_DELIMITER)
+            if (sourceDelimIdx != -1) {
+                id = recordRefStr.substring(sourceDelimIdx + 1)
+                val source = recordRefStr.substring(0, sourceDelimIdx)
+                val appNameDelimIdx = source.indexOf(APP_NAME_DELIMITER)
+                if (appNameDelimIdx > -1) {
+                    appName = source.substring(0, appNameDelimIdx)
+                    sourceId = source.substring(appNameDelimIdx + 1)
+                } else {
+                    appName = ""
+                    sourceId = source
+                }
+            } else {
+                id = recordRefStr
+                sourceId = ""
+                appName = ""
+            }
+            return create(appName, sourceId, id)
+        }
+
+        @JvmStatic
+        fun valueOf(ref: RecordRef?): RecordRef {
+            return ref ?: EMPTY
+        }
+
+        @JvmStatic
+        private fun isBlankId(id: String?): Boolean {
+            return isBlank(id) || containsOnly(id!!, '@')
+        }
+
+        @JvmStatic
+        fun toString(ref: RecordRef?): String {
+            return ref?.toString() ?: ""
+        }
+    }
+
+    val appName: String
+    val sourceId: String
+    val id: String
+
+    private constructor() {
+        appName = ""
+        sourceId = ""
+        id = ""
+    }
+
+    private constructor(appName: String, sourceId: String, id: String) {
+        this.id = if (isNotBlank(id)) id else ""
+        this.appName = if (isNotBlank(appName)) appName else ""
+        this.sourceId = if (isNotBlank(sourceId)) sourceId else ""
+    }
+
+    fun addAppName(appName: String): RecordRef {
+        val current = toString()
+        return if (current.contains("@")) {
+            valueOf("$appName/$current")
         } else {
-            id = recordRefStr;
-            sourceId = "";
-            appName = "";
+            valueOf("$appName/@$current")
         }
-
-        return create(appName, sourceId, id);
     }
 
-    @NotNull
-    public static RecordRef valueOf(RecordRef ref) {
-        return ref != null ? ref : EMPTY;
-    }
-
-    @NotNull
-    public String getSourceId() {
-        return sourceId;
-    }
-
-    @NotNull
-    public String getId() {
-        return id;
-    }
-
-    @NotNull
-    public String getAppName() {
-        return appName;
-    }
-
-    public boolean isRemote() {
-        return !appName.isEmpty();
-    }
-
-    private static boolean isBlankId(String id) {
-        return StringUtils.isBlank(id) || StringUtils.containsOnly(id, '@');
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
+    fun withDefaultAppName(appName: String): RecordRef {
+        if (isBlank(appName)) {
+            return this
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        RecordRef that = (RecordRef) o;
-        return Objects.equals(appName, that.appName)
-            && Objects.equals(sourceId, that.sourceId)
-            && Objects.equals(id, that.id);
+        return if (!this.appName.isEmpty()) this else addAppName(appName)
     }
 
-    @Override
-    public int hashCode() {
-        int result = Objects.hashCode(sourceId);
-        result = 31 * result + Objects.hashCode(id);
-        result = 31 * result + Objects.hashCode(appName);
-        return result;
+    fun removeAppName(): RecordRef {
+        return if (appName.isEmpty()) {
+            this
+        } else create(sourceId, id)
+    }
+
+    fun isRemote() : Boolean {
+        return appName.isNotEmpty()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (other == null || javaClass != other.javaClass) {
+            return false
+        }
+        val that = other as RecordRef
+        return (appName == that.appName
+            && sourceId == that.sourceId
+            && id == that.id)
+    }
+
+    override fun hashCode(): Int {
+        var result = Objects.hashCode(sourceId)
+        result = 31 * result + Objects.hashCode(id)
+        result = 31 * result + Objects.hashCode(appName)
+        return result
     }
 
     @JsonValue
     @com.fasterxml.jackson.annotation.JsonValue
-    @Override
-    public String toString() {
-        if (this == EMPTY) {
-            return "";
+    override fun toString(): String {
+        if (this === EMPTY) {
+            return ""
         }
-        if (appName.length() > 0) {
-            return appName + APP_NAME_DELIMITER + sourceId + SOURCE_DELIMITER + id;
+        if (appName.isNotEmpty()) {
+            return appName + APP_NAME_DELIMITER + sourceId + SOURCE_DELIMITER + id
         }
-        if (sourceId.length() > 0) {
-            return sourceId + SOURCE_DELIMITER + id;
+        return if (sourceId.isNotEmpty()) {
+            sourceId + SOURCE_DELIMITER + id
+        } else {
+            id
         }
-        return id;
-    }
-
-    public static String toString(RecordRef ref) {
-        return ref == null ? "" : ref.toString();
     }
 }

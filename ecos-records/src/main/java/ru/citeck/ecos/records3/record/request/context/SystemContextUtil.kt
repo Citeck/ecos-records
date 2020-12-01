c0b8f2e6117d1822f9a3b7d61282d97d957fb151
Pavel Simonov <pavel.simonov@citeck.ru>
2020-12-01 10:56:54 +0700

    Add SystemContextUtil

--- ecos-records/src/main/java/ru/citeck/ecos/records3/record/request/context/SystemContextUtil.kt
+++ ecos-records/src/main/java/ru/citeck/ecos/records3/record/request/context/SystemContextUtil.kt
@@ -7,26 +7,62 @@ object SystemContextUtil {
 
     private const val SYSTEM_CONTEXT_KEY = "is-system-context"
 
-    fun <T : Any> doAsSystem(action: () -> T?): T? {
-        return RequestContext.getCurrentNotNull().doWithVar(SYSTEM_CONTEXT_KEY, true, action)
+    @JvmStatic
+    @JvmOverloads
+    fun <T : Any> doAsSystemIfNotSystemContextJ(
+        action: Supplier<T>,
+        systemCtxValue: T,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T {
+        return doAsSystemIfNotSystemContext({ action.get() }, systemCtxValue, context)
     }
 
-    fun <T : Any> doAsSystemNotNull(action: () -> T): T {
-        return RequestContext.getCurrentNotNull().doWithVarNotNull(SYSTEM_CONTEXT_KEY, true, action)
+    fun <T : Any> doAsSystemIfNotSystemContext(
+        action: () -> T,
+        systemCtxValue: T,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T {
+        if (isSystemContext(context)) {
+            return systemCtxValue
+        }
+        return doAsSystemNotNull(action)
+    }
+
+    fun <T : Any> doAsSystem(
+        action: () -> T?,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T? {
+        return context.doWithVar(SYSTEM_CONTEXT_KEY, true, action)
+    }
+
+    fun <T : Any> doAsSystemNotNull(
+        action: () -> T,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T {
+        return context.doWithVarNotNull(SYSTEM_CONTEXT_KEY, true, action)
     }
 
     @JvmStatic
-    fun <T : Any> doAsSystemJ(action: Supplier<T?>): T? {
-        return doAsSystem { action.get() }
+    @JvmOverloads
+    fun <T : Any> doAsSystemJ(
+        action: Supplier<T?>,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T? {
+        return doAsSystem({ action.get() }, context)
     }
 
     @JvmStatic
-    fun <T : Any> doAsSystemNotNullJ(action: Supplier<T>): T {
-        return doAsSystemNotNull { action.get() }
+    @JvmOverloads
+    fun <T : Any> doAsSystemNotNullJ(
+        action: Supplier<T>,
+        context: RequestContext = RequestContext.getCurrentNotNull()
+    ): T {
+        return doAsSystemNotNull({ action.get() }, context)
     }
 
     @JvmStatic
-    fun isSystemContext(): Boolean {
-        return RequestContext.getCurrentNotNull().getVar<Boolean>(SYSTEM_CONTEXT_KEY) == true
+    @JvmOverloads
+    fun isSystemContext(context: RequestContext = RequestContext.getCurrentNotNull()): Boolean {
+        return context.getVar<Boolean>(SYSTEM_CONTEXT_KEY) == true
     }
 }

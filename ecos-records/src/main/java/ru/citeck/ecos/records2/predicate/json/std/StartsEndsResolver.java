e5961d06ddcb17104bc8e5afa6e05aa9f7b200a6
Pavel Simonov <pavel.simonov@citeck.ru>
2020-12-03 09:42:24 +0700

    Fix jsonMapper.applyData with predicates

--- ecos-records/src/main/java/ru/citeck/ecos/records2/predicate/json/std/StartsEndsResolver.java
+++ ecos-records/src/main/java/ru/citeck/ecos/records2/predicate/json/std/StartsEndsResolver.java
@@ -1,7 +1,7 @@
 package ru.citeck.ecos.records2.predicate.json.std;
 
 import ecos.com.fasterxml.jackson210.core.JsonProcessingException;
-import ecos.com.fasterxml.jackson210.databind.ObjectMapper;
+import ecos.com.fasterxml.jackson210.core.ObjectCodec;
 import ecos.com.fasterxml.jackson210.databind.node.ObjectNode;
 import ru.citeck.ecos.records2.predicate.model.Predicate;
 
@@ -11,7 +11,7 @@ import java.util.List;
 public class StartsEndsResolver implements PredicateResolver {
 
     @Override
-    public Predicate resolve(ObjectMapper mapper, ObjectNode node) throws JsonProcessingException {
+    public Predicate resolve(ObjectCodec mapper, ObjectNode node) throws JsonProcessingException {
 
         String type = node.get("t").asText();
 

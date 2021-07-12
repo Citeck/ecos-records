2859fa4aee9c03576e6be5c4c2ce8aaf56bf9081
Pavel Simonov <pavel.simonov@citeck.ru>
2021-07-12 09:03:15 +0700

    Improve auth interceptor

--- ecos-records-spring/src/main/java/ru/citeck/ecos/records3/spring/web/interceptor/AuthHeaderProvider.kt
+++ ecos-records-spring/src/main/java/ru/citeck/ecos/records3/spring/web/interceptor/AuthHeaderProvider.kt
@@ -3,4 +3,6 @@ package ru.citeck.ecos.records3.spring.web.interceptor
 interface AuthHeaderProvider {
 
     fun getAuthHeader(userName: String): String?
+
+    fun getSystemAuthHeader(userName: String): String?
 }

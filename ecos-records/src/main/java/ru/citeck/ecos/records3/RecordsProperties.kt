b54f0fc9427c0edc75783d0c9896dd8787a3e27d
Pavel Simonov <pavel.simonov@citeck.ru>
2022-05-12 08:37:32 +0700

    ECOSCOM-4664 - ECOS WebApp interfaces

--- ecos-records/src/main/java/ru/citeck/ecos/records3/RecordsProperties.kt
+++ ecos-records/src/main/java/ru/citeck/ecos/records3/RecordsProperties.kt
@@ -1,12 +1,6 @@
 package ru.citeck.ecos.records3
 
 class RecordsProperties {
-
-    @Deprecated("use EcosWebAppContext.getProperties().appName")
-    var appName = ""
-    @Deprecated("use EcosWebAppContext.getProperties().appInstanceId")
-    var appInstanceId = ""
     var sourceIdMapping: Map<String, String> = emptyMap()
-
     var defaultApp: String = ""
 }

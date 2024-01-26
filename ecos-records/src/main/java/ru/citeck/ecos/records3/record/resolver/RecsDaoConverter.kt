beb0ecfbe98cce4067d3f95116e5548b7f5002bd
Pavel Simonov <pavel.simonov@citeck.ru>
2024-01-26 12:57:54 +0700

    ECOSCOM-5388 - Remove error catching in single->multiple records DAO adapters. Release 3.66.3

--- ecos-records/src/main/java/ru/citeck/ecos/records3/record/resolver/RecsDaoConverter.kt
+++ ecos-records/src/main/java/ru/citeck/ecos/records3/record/resolver/RecsDaoConverter.kt
@@ -6,7 +6,6 @@ import ru.citeck.ecos.commons.data.DataValue
 import ru.citeck.ecos.commons.data.ObjectData
 import ru.citeck.ecos.commons.utils.ReflectUtils
 import ru.citeck.ecos.records2.RecordRef
-import ru.citeck.ecos.records2.request.error.ErrorUtils
 import ru.citeck.ecos.records3.record.atts.dto.LocalRecordAtts
 import ru.citeck.ecos.records3.record.atts.value.factory.bean.BeanTypeUtils
 import ru.citeck.ecos.records3.record.atts.value.impl.EmptyAttValue
@@ -21,8 +20,6 @@ import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
 import ru.citeck.ecos.records3.record.dao.query.RecordsQueryResDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
 import ru.citeck.ecos.records3.record.dao.query.dto.res.RecsQueryRes
-import ru.citeck.ecos.records3.record.request.RequestContext
-import ru.citeck.ecos.records3.record.request.msg.MsgLevel
 import java.util.*
 
 class RecsDaoConverter(
@@ -200,8 +197,7 @@ class RecsDaoConverter(
                 return mapElements(
                     recordIds,
                     { dao.getRecordAtts(it) },
-                    { EmptyAttValue.INSTANCE },
-                    { _, _ -> ObjectData.create() }
+                    { EmptyAttValue.INSTANCE }
                 )
             }
             override fun getId(): String = dao.getId()
@@ -216,8 +212,7 @@ class RecsDaoConverter(
                 return mapElements(
                     recordIds,
                     { dao.delete(it) },
-                    { DelStatus.OK },
-                    { _, e -> throw e }
+                    { DelStatus.OK }
                 )
             }
 
@@ -248,26 +243,16 @@ class RecsDaoConverter(
     private fun <T, R> mapElements(
         input: List<T>,
         mapFunc: (T) -> R,
-        onEmpty: (T) -> R,
-        onError: (T, Throwable) -> R
+        onEmpty: (T) -> R
     ): List<R> {
 
         val result: MutableList<R> = ArrayList()
         for (value in input) {
-            try {
-                var res = mapFunc.invoke(value)
-                if (res == null) {
-                    res = onEmpty.invoke(value)
-                }
-                result.add(res)
-            } catch (e: Throwable) {
-                log.error("Mapping failed", e)
-                val context = RequestContext.getCurrentNotNull()
-                context.addMsg(MsgLevel.ERROR) {
-                    ErrorUtils.convertException(e, context.getServices())
-                }
-                result.add(onError.invoke(value, e))
+            var res = mapFunc.invoke(value)
+            if (res == null) {
+                res = onEmpty.invoke(value)
             }
+            result.add(res)
         }
         return result
     }
